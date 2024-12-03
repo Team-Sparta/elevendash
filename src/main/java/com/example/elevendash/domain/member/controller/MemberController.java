f4d382e4b67536e1e6857a96f99a0eea70d99869
Youngjun Kim <158126791+Youngjun97@users.noreply.github.com>
2024-12-03 15:39:53 +0900

    ED-1: test jira connection

---
+++ src/main/java/com/example/elevendash/domain/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.example.elevendash.domain.member.controller;
+
+public class MemberController {
+}
---
+++ src/main/java/com/example/elevendash/domain/member/service/MemberService.java
@@ -0,0 +1,4 @@
+package com.example.elevendash.domain.member.service;
+
+public class MemberService {
+}
