bc63246ca09c1e344e4cd0496e63877f9a943b7d
thdwj <thdwjdgkr123@naver.com>
2024-12-04 22:44:49 +0900

    git commit -m "ED-20 <메뉴 옵션 삭제 기능 구현>"

--- src/main/java/com/example/elevendash/domain/menu/repository/MenuOptionRepository.java
+++ src/main/java/com/example/elevendash/domain/menu/repository/MenuOptionRepository.java
@@ -1,7 +1,9 @@
 package com.example.elevendash.domain.menu.repository;
 
+import com.example.elevendash.domain.menu.entity.Menu;
 import com.example.elevendash.domain.menu.entity.MenuOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuOptionRepository extends JpaRepository<MenuOption, Long> {
+    Long menu(Menu menu);
 }
