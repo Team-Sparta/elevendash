06afaae8ef1be98ee167a6cebc918e1dc574080a
kangjunhyuk <kangjun1290@gmail.com>
2024-12-05 22:31:03 +0900

    Fix : 코드 수정
    Cart의 매개변수 변경으로 인한 코드 변경

--- src/main/java/com/example/elevendash/domain/cart/service/CartService.java
+++ src/main/java/com/example/elevendash/domain/cart/service/CartService.java
@@ -8,47 +8,55 @@ import com.example.elevendash.domain.order.repository.OrderRepository;
 import com.example.elevendash.domain.store.entity.Store;
 import com.example.elevendash.domain.store.repository.StoreRepository;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
+@RequiredArgsConstructor
 @Service
 public class CartService {
     private final StoreRepository storeRepository;
     private final OrderRepository orderRepository;
 
-    public CartService(StoreRepository storeRepository, OrderRepository orderRepository) {
-        this.storeRepository = storeRepository;
-        this.orderRepository = orderRepository;
-    }
 
-    public CartResponseDto createCookie(Long orderId, HttpServletResponse response, CartRequestDto requestDto) {
-        Order order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("잘못된 Id 값입니다"));
-        List<Menu> orderMenuName = requestDto.getMenuName();
-        Store storeId = findstoreByOrder(orderId);
-        if (!order.getStore().getId().toString().equals(storeId.toString())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "올바르지 못한 요청입니다");
-        }
+    public CartResponseDto createCookie(HttpServletResponse response, HttpServletRequest request, CartRequestDto requestDto) {
+        List<Long> menus = new ArrayList<>(requestDto.getMenuId());
+        Long storeId = requestDto.getStoreId();
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("잘못된 ID 값입니다"));
+        List<String> orderSuccess = new ArrayList<>();
+        List<Menu> storeMenu = store.getMenus();
+        Integer price;
 
-        List<Menu> orderSuccess = new ArrayList<>();
+        // 클라이언트가 보내준 menu가 store에 있는 menu인지 확인
+        for (Menu menu : storeMenu) {
+            for (String food : menus) {
+                if (storeMenu.stream().map(Objects::toString).toList().contains(food)) {
+                    orderSuccess.add(food);
+                    price = menu.getMenuPrice();
 
-            for (Menu orderMenu : orderMenuName) {
-                if (storeId.getMenus().contains(orderMenu)) {
-                    orderSuccess.add(orderMenu);
-                }else {
+                } else {
                     throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않은 매뉴입니다");
+                }
             }
         }
+
+
         Cookie cookie = new Cookie("menuName", String.valueOf(requestDto));
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
         response.addCookie(cookie);
-        return new CartResponseDto(orderId, orderSuccess);
+        return new CartResponseDto(orderSuccess);
     }
+
+
 
     public Store findstoreByOrder(Long orderId) {
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new IllegalArgumentException("잘못된 Id 값입니다"));
