06794d65d8dd065a1ad7362e3a43ee16a1ec2c91
thdwj <thdwjdgkr123@naver.com>
2024-12-06 00:56:39 +0900

    "ED-55 <광고 띄우는 기능 구현>"

--- src/main/java/com/example/elevendash/domain/advertisement/dto/request/UpdateAdvertisementRequestDto.java
+++ src/main/java/com/example/elevendash/domain/advertisement/dto/request/UpdateAdvertisementRequestDto.java
@@ -1,4 +1,15 @@
 package com.example.elevendash.domain.advertisement.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class UpdateAdvertisementRequestDto {
+    @NotNull
+    @Min(1)
+    private final Integer bidPrice;
 }
