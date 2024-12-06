633bf679ed6bcfab2fb773727833342db6f94585
kangjunhyuk <kangjun1290@gmail.com>
2024-12-06 13:43:23 +0900

    Feat : 주문 상태 변경 밑 거절 기능생성
    member가 OWNER 상태일때만 변경 가능하도록 하고 클라이언트가 전해준 주문 상태에 따라 switch로 변경하도록 설정 주문 거절 상태일 경우 예외발생

--- src/main/java/com/example/elevendash/domain/order/service/OrderService.java
+++ src/main/java/com/example/elevendash/domain/order/service/OrderService.java
@@ -2,7 +2,11 @@ package com.example.elevendash.domain.order.service;
 
 import com.example.elevendash.domain.dashboard.dto.response.StatisticsResponse;
 import com.example.elevendash.domain.dashboard.enums.PeriodType;
+import com.example.elevendash.domain.member.entity.Member;
 import com.example.elevendash.domain.menu.enums.Categories;
+import com.example.elevendash.domain.order.dto.request.CancelOrderRequestDto;
+import com.example.elevendash.domain.order.dto.request.orderStatusRequestDto;
+import com.example.elevendash.domain.order.dto.response.CancelOrderResponseDto;
 import com.example.elevendash.domain.order.dto.response.OrderCheckResponseDto;
 import com.example.elevendash.domain.order.entity.Order;
 import com.example.elevendash.domain.order.repository.OrderRepository;
@@ -13,6 +17,8 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+
+import static com.example.elevendash.domain.member.enums.MemberRole.OWNER;
 
 @Service
 public class OrderService {
@@ -65,5 +71,54 @@ public class OrderService {
                 endDate.atTime(23, 59, 59).atZone(ZoneId.systemDefault()).toLocalDateTime() : null;
 
         return orderRepository.getStatistics(startDateTime, endDateTime, storeId);
+    }
+
+    public String orderStatus (Long ordersId, orderStatusRequestDto requestDto) {
+        Order order = findOrderById(ordersId);
+
+        Member member = order.getMember();
+
+        if (!member.getRole().equals(OWNER)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "사장님만 이용 가능합니다");
+        }
+
+        String orderStatus = requestDto.getOrderStatus();
+
+        if (order.getOrderStatus().equals(orderStatus)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "같은 상태로는 변경 불가능합니다");
+        }
+
+        if (order.getOrderStatus().equals("주문 거절")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "거절한 주문입니다");
+        }
+
+        switch (orderStatus) {
+            case "주문 수락" -> {
+                order.updateStatus("주문 수락");
+            }
+            case "조리중" -> {
+                order.updateStatus("조리중");
+            }
+            case "조리 완료" -> {
+                order.updateStatus("조리 완료");
+            }
+            case "배달중" -> {
+                order.updateStatus("배달중");
+            }
+            case "배달 완료" -> {
+                order.updateStatus("배달 완료");
+            }
+            default -> {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못 입력하였습니다 다시 시도해주세요");
+            }
+        }
+
+        return "변경이 완료되었습니다";
+    }
+
+    public CancelOrderResponseDto cancelOrder (Long ordersId, CancelOrderRequestDto requestDto) {
+        Order order = findOrderById(ordersId);
+        order.updateStatus("주문 거절");
+        return new CancelOrderResponseDto(order.getOrderStatus(), requestDto.getCancelMassage());
     }
 }
