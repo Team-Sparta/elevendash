1f0121c59975cdbfda2795651b0b64c2f4281673
kangjunhyuk <kangjun1290@gmail.com>
2024-12-07 00:00:06 +0900

    Feat : order 생성
    장바구니에서 고른 menu와 storeId 있는 쿠키에서 값을 가져와 price와 menuId를 구하고 값을 저장

--- src/main/java/com/example/elevendash/domain/order/service/OrderService.java
+++ src/main/java/com/example/elevendash/domain/order/service/OrderService.java
@@ -1,32 +1,71 @@
 package com.example.elevendash.domain.order.service;
 
+import com.example.elevendash.domain.cart.dto.response.CartCookieResponseDto;
+import com.example.elevendash.domain.coupon.entity.Coupon;
+import com.example.elevendash.domain.coupon.entity.CouponUsage;
+import com.example.elevendash.domain.coupon.repository.CouponRepository;
+import com.example.elevendash.domain.coupon.service.CouponService;
 import com.example.elevendash.domain.dashboard.dto.response.StatisticsResponse;
 import com.example.elevendash.domain.dashboard.enums.PeriodType;
 import com.example.elevendash.domain.member.entity.Member;
+import com.example.elevendash.domain.member.repository.MemberRepository;
+import com.example.elevendash.domain.menu.entity.Menu;
 import com.example.elevendash.domain.menu.enums.Categories;
+import com.example.elevendash.domain.menu.repository.MenuRepository;
+import com.example.elevendash.domain.order.dto.request.AddOrderRequestDto;
 import com.example.elevendash.domain.order.dto.request.CancelOrderRequestDto;
 import com.example.elevendash.domain.order.dto.request.orderStatusRequestDto;
+import com.example.elevendash.domain.order.dto.response.AddOrderResponseDto;
 import com.example.elevendash.domain.order.dto.response.CancelOrderResponseDto;
 import com.example.elevendash.domain.order.dto.response.OrderCheckResponseDto;
 import com.example.elevendash.domain.order.entity.Order;
 import com.example.elevendash.domain.order.repository.OrderRepository;
+import com.example.elevendash.domain.point.repository.PointRepository;
+import com.example.elevendash.domain.point.service.PointService;
+import com.example.elevendash.domain.store.entity.Store;
+import com.example.elevendash.domain.store.repository.StoreRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.hibernate.validator.internal.constraintvalidators.bv.time.futureorpresent.FutureOrPresentValidatorForOffsetDateTime;
+import org.springframework.cloud.bootstrap.encrypt.KeyProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.*;
 
 import static com.example.elevendash.domain.member.enums.MemberRole.OWNER;
+import static com.example.elevendash.domain.member.enums.MemberRole.valueOf;
 
 @Service
 public class OrderService {
 
+    private final ObjectMapper jacksonObjectMapper;
     private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
+    private final CouponRepository couponRepository;
+    private final CouponService couponService;
+    private final PointRepository pointRepository;
 
-    public OrderService(OrderRepository orderService) {
+
+    public OrderService(ObjectMapper jacksonObjectMapper, OrderRepository orderService, MemberRepository memberRepository, MenuRepository menuRepository, StoreRepository storeRepository, CouponRepository couponRepository, CouponService couponService, PointRepository pointRepository) {
+        this.jacksonObjectMapper = jacksonObjectMapper;
         this.orderRepository = orderService;
+        this.memberRepository = memberRepository;
+        this.menuRepository = menuRepository;
+        this.storeRepository = storeRepository;
+        this.couponRepository = couponRepository;
+        this.couponService = couponService;
+        this.pointRepository = pointRepository;
+
     }
 
     public Order findOrderById(Long ordersId) {
@@ -120,5 +159,60 @@ public class OrderService {
         Order order = findOrderById(ordersId);
         order.updateStatus("주문 거절");
         return new CancelOrderResponseDto(order.getOrderStatus(), requestDto.getCancelMassage());
+    }
+
+    public AddOrderResponseDto addOrder (AddOrderRequestDto requestDto, HttpServletRequest request) throws JsonProcessingException {
+        Long memberId = requestDto.getMemberId();
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new IllegalArgumentException("잘못된 Id 값입니다"));
+        Long menuPrice = 0L;
+        BigDecimal totalPrice = new BigDecimal(0);
+
+        List<Menu> menus = new ArrayList<>();
+        List<Long> manuCount = new ArrayList<>();
+
+        Long storeId = 0L;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("menuList")) {
+                Map<String, CartCookieResponseDto> menuList = jacksonObjectMapper.readValue(cookie.getValue(), Map.class);
+                for (String key : menuList.keySet()) {
+                    CartCookieResponseDto cartCookieResponseDto = menuList.get(key);
+                    menus.addAll(cartCookieResponseDto.getMenus());
+                    manuCount.addAll(cartCookieResponseDto.getMenuCount());
+
+                }
+            }else if (cookie.getName().equals("store_id")) {
+                Long storeIda = jacksonObjectMapper.readValue(cookie.getValue(), Long.class);
+                storeId = storeIda;
+            }
+        }
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new IllegalArgumentException("잘못된 ID 값입니다"));
+
+
+
+        // 금액 구하는 for문
+        for (Menu singleMenuId : menus) {
+            Menu menu = menuRepository.findById(singleMenuId.getId()).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 Id 값입니다"));
+            menuPrice = Long.valueOf(menu.getMenuPrice());
+        }
+
+        // 구한 금액을 쿠폰에 적용
+        List<CouponUsage> membercoupon = member.getCoupons();
+        for (CouponUsage singleCoupon : membercoupon) {
+            if (singleCoupon.getId().equals(requestDto.getCouponId())) {
+                Coupon coupon = couponRepository.findById(singleCoupon.getId()).orElseThrow(() -> new IllegalArgumentException("유효하지 않은 Id 값입니다"));
+                BigDecimal bigDecimalPrice = BigDecimal.valueOf(menuPrice);
+                //할인 쿠폰 적용 (BigDecimal로 반환)
+                totalPrice = couponService.calculateDiscount(coupon, bigDecimalPrice);
+                singleCoupon.useCoupon();
+            }
+        }
+
+        // BigDecimal 타입인 totalPrice 를 Long 타입으로 변경
+        Long LongTotalPrice = Long.valueOf(String.valueOf(totalPrice));
+
+        Order order = new Order(LongTotalPrice, menus, member, store);
+        Order saveOrder = orderRepository.save(order);
+        return AddOrderResponseDto.toDto(saveOrder);
     }
 }
