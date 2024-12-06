20ce0c7f8b35a559ba319748bc8c8876ee09c2f0
예지 <lee24pm@gmail.com>
2024-12-06 14:37:52 +0900

    fix: 예외처리 메서드 추출

--- src/main/java/com/example/elevendash/domain/review/service/ReviewService.java
+++ src/main/java/com/example/elevendash/domain/review/service/ReviewService.java
@@ -1,6 +1,8 @@
 package com.example.elevendash.domain.review.service;
 
+import com.example.elevendash.domain.comment.entity.Comment;
 import com.example.elevendash.domain.member.entity.Member;
+import com.example.elevendash.domain.order.entity.Order;
 import com.example.elevendash.domain.order.repository.OrderRepository;
 import com.example.elevendash.domain.review.dto.request.CreateReviewDto;
 import com.example.elevendash.domain.review.dto.request.UpdateReviewDto;
@@ -25,64 +27,60 @@ public class ReviewService {
     private final ReviewRepository reviewRepository;
 
     public ReviewResponseDto create(Long storeId, CreateReviewDto dto){
-//        Order findOrder = orderRepository.findByIdOrElseThrow(dto.getOrderId());
-//        if(!findOrder.getOrderStatus().equals("배달완료")){
-//            throw new BaseException(ErrorCode.NOT_DELIVERED);
-//        }
-//        Review review = new Review(dto); //dto: orderId, content, starRating
-//        Review savedReview = reviewRepository.save(review);
-//
-//        return new ReviewResponseDto(savedReview);
-        return null;
-    }
+        Order findOrder = orderRepository.findByIdOrElseThrow(dto.getOrderId());
+        if(!findOrder.getOrderStatus().equals("배달완료")){
+            throw new BaseException(ErrorCode.NOT_DELIVERED);
+        }
+        Review review = new Review(dto, findOrder);
+        Review savedReview = reviewRepository.save(review);
 
-    private final int DEFAULT_PAGE_SIZE = 2;
-
-    private Pageable pageable(int page) {
-        return PageRequest.of(page, DEFAULT_PAGE_SIZE, Sort.by(Sort.Direction.DESC, "createdAt"));
+        return new ReviewResponseDto(savedReview);
     }
 
     public Page<PageReviewResponseDto> find(Long storeId, Long memberId, Pageable pageable){
 
-        Page<PageReviewResponseDto> reviews = reviewRepository.findByStore_IdAndMemberNotPage(storeId, memberId, pageable).map(review -> new PageReviewResponseDto(review, review.getComment()));
+        Page<PageReviewResponseDto> reviews = reviewRepository.findByStore_IdAndMemberNotPage(storeId, memberId, pageable)
+                .map(review -> new PageReviewResponseDto(review, review.getComment()));
 
         return reviews;
     }
 
-    public Page<PageReviewResponseDto> findBystarRating(Long storeId, int minStar, int maxStar, int page){
+    public Page<PageReviewResponseDto> findBystarRating(Long storeId, int minStar, int maxStar, Pageable pageable){
 
         if(minStar > maxStar) {
             throw new BaseException(ErrorCode.BAD_STARRATING);
         }
-        Pageable pageable = pageable(page);
-        Page<PageReviewResponseDto> reviews = reviewRepository.findByStarRating(storeId, minStar, maxStar, pageable).map(review -> new PageReviewResponseDto(review, review.getComment()));
+        Page<PageReviewResponseDto> reviews = reviewRepository.findByStarRating(storeId, minStar, maxStar, pageable)
+                .map(review -> new PageReviewResponseDto(review, review.getComment()));
 
         return reviews;
     }
 
+    @Transactional
     public ReviewResponseDto updateReview(Long reviewId, Long loginMemberId, UpdateReviewDto dto){
         Review findReview = reviewRepository.findByIdOrElseThrow(reviewId);
 
-        if (!findReview.getMember().getId().equals(loginMemberId)) {
-            throw new BaseException(ErrorCode.UNAUTHORIZED_REVIEW_UPDATE);
-        }
+        validateMember(loginMemberId, findReview);
 
         findReview.updateReview(dto);
         Review savedReview = reviewRepository.save(findReview);
 
         return new ReviewResponseDto(savedReview);
-
     }
 
     @Transactional
     public void delete(Long reviewId, Long loginMemberId) {
         Review findReview = reviewRepository.findByIdOrElseThrow(reviewId);
 
-        if (!findReview.getMember().getId().equals(loginMemberId)) {
-            throw new BaseException(ErrorCode.UNAUTHORIZED_REVIEW_DELETE);
-        }
+        validateMember(loginMemberId, findReview);
 
         reviewRepository.delete(findReview);
+    }
+
+    private void validateMember(Long loginMemberId, Review findReview){
+        if (!findReview.getMember().getId().equals(loginMemberId)) {
+            throw new BaseException(ErrorCode.UNAUTHORIZED_REVIEW);
+        }
     }
 
 }
